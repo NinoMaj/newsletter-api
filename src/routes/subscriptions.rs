93525afb0041ae7a7db15254f53d080908045847
Nino Majder <nino.majder@gmail.com>
2021-10-13 21:56:31 +0200

    Add insert query

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,7 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
 use sqlx::PgConnection;
+use uuid::Uuid;
 
 #[derive(serde:: Deserialize)]
 pub struct FormData {
@@ -8,8 +10,21 @@ pub struct FormData {
 }
 
 pub async fn subscribe(
-    _form: web::Form<FormData>,
-    _connection: web::Data<PgConnection>,
+    form: web::Form<FormData>,
+    connection: web::Data<PgConnection>,
 ) -> HttpResponse {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // We use `get_ref` to get an immutable reference to the `PgConnection` // wrapped by `web::Data`.
+    .execute(connection.get_ref())
+    .await;
+
     HttpResponse::Ok().finish()
 }
