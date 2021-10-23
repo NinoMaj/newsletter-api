03023796d2bda1a946a844a10a24998b829f427f
Nino Majder <nino.majder@gmail.com>
2021-10-23 20:57:42 +0200

    Improve db configuration

--- tests/subscriptions.rs
+++ tests/subscriptions.rs
@@ -2,6 +2,7 @@ use newsletter_api::configuration::get_configuration;
 use newsletter_api::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
@@ -12,7 +13,9 @@ async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
-    let configuration = get_configuration().expect("Failed to read configuration.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
