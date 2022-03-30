ea5244378b59ea337f38e38692d64ade95e945e3
Nino Majder <nino.majder@gmail.com>
2022-03-30 10:49:09 +0200

    Implement all basic newsletter features

---
+++ README.md
@@ -0,0 +1,72 @@
+# newsletter-api
+
+## Pre-requisites
+
+You'll need to install:
+
+- [Rust](https://www.rust-lang.org/tools/install)
+- [Docker](https://docs.docker.com/get-docker/)
+
+There are also some OS-specific requirements.
+
+### Windows
+
+```bash
+cargo install -f cargo-binutils
+rustup component add llvm-tools-preview
+```
+
+### Linux
+
+```bash
+# Ubuntu
+sudo apt-get install lld clang
+# Arch
+sudo pacman -S lld clang
+```
+
+### MacOS
+
+```bash
+brew install michaeleisel/zld/zld
+```
+
+## How to build
+
+Launch a (migrated) Postgres database via Docker:
+
+```bash
+./scripts/init_db.sh
+```
+
+Launch a Redis instance via Docker:
+
+```bash
+./scripts/init_redis.sh
+```
+
+Launch `cargo`:
+
+```bash
+cargo build
+```
+
+## How to test
+
+Launch a (migrated) Postgres database via Docker:
+
+```bash
+./scripts/init_db.sh
+```
+
+Launch a Redis instance via Docker:
+
+```bash
+./scripts/init_redis.sh
+```
+
+Launch `cargo`:
+
+```bash
+cargo test
+```
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,48 @@
-use env_logger::Env;
 use newsletter_api::configuration::get_configuration;
-use newsletter_api::startup::run;
-use sqlx::PgPool;
-use std::net::TcpListener;
+use newsletter_api::issue_delivery_worker::run_worker_until_stopped;
+use newsletter_api::startup::Application;
+use newsletter_api::telemetry::{get_subscriber, init_subscriber};
+use std::fmt::{Debug, Display};
+use tokio::task::JoinError;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let subscriber = get_subscriber("newsletter_api".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    let application = Application::build(configuration.clone()).await?;
+    let application_task = tokio::spawn(application.run_until_stopped());
+    let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
+
+    tokio::select! {
+        o = application_task => report_exit("API", o),
+        o = worker_task =>  report_exit("Background worker", o),
+    };
+
+    Ok(())
+}
+
+fn report_exit(task_name: &str, outcome: Result<Result<(), impl Debug + Display>, JoinError>) {
+    match outcome {
+        Ok(Ok(())) => {
+            tracing::info!("{} has exited", task_name)
+        }
+        Ok(Err(e)) => {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "{} failed",
+                task_name
+            )
+        }
+        Err(e) => {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "{}' task failed to complete",
+                task_name
+            )
+        }
+    }
 }
